607a7713583753b604a7c410cad10502c90b8bab
Albert24GG <guiman.albert09@gmail.com>
2024-02-17 15:59:27 +0200

    Add vim-fugitive

--- plugins.lua
+++ plugins.lua
@@ -198,17 +198,7 @@ return {
 		end,
 	},
 
-	-- To make a plugin not be loaded
-	-- {
-	--   "NvChad/nvim-colorizer.lua",
-	--   enabled = false
-	-- },
-
-	-- All NvChad plugins are lazy-loaded by default
-	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-	-- {
-	--   "mg979/vim-visual-multi",
-	--   lazy = false,
-	-- }
+	{
+		"tpope/vim-fugitive",
+	},
 }
